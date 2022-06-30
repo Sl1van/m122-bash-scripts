a99c73dc2768968ce4ad4afd18e731eefab85794
Sl1van <sl1van@sl1van.com>
2022-06-30 17:42:40 +0000

    added more scripts

--- aufgaben17-20.sh
+++ aufgaben17-20.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 # Kommentieren Sie die Zeilen aus die sie ausführen wollen.
 
-# Aufgabe 17
-# Aufgabe 18
+# # Aufgabe 17
+# rand=$((RANDOM%100))
+# echo $rand
+# echo $rand | logger -p user.info
+
+# # Aufgabe 18
+# function random_number {
+#   rand=$((RANDOM % 100))
+#   echo $rand
+#   logger -p user.info -i -t randomly $rand
+# }
+# random_number
+# random_number
+# random_number
 
 # # Aufgabe 19
 # function executeCommandAndExitOnError {
