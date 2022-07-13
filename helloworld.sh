d2cfb2ce3b705358877b640988985d08e6596da0
Sl1van <sl1van@sl1van.com>
2022-07-13 21:41:07 +0200

    added old scripts

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#/!/bin/bash
+
+echo "Hello world"
+printf "\n\n"
+
+for i in {1..100}
+do
+   echo "Hello world $i times"
+done
+
+printf "\n\n"
+
+mins=$(date +"%M")
+for (( min=1; min<=mins; min++ ))
+do
+   echo "Hello world $min times"
+done
---
+++ numbers_guesser.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+random_number=$((RANDOM%15+1))
+
+echo "Guess a input between 1 and 15:"
+read input
+
+while [ $input -ne $random_number ]
+do
+  if [ $input -gt $random_number ]
+  then
+    echo "Your guess is higher than the number input"
+  else
+    echo "Your guess is lower than the number input"
+  fi
+  echo "Guess again:"
+  read input
+done
+echo "You guessed the correct input"
